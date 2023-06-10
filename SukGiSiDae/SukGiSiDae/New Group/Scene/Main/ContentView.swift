02e96ef8b45a38c27c3f779933e45de21374de0c
ChangMin <sopg04@gmail.com>
2023-06-10 16:08:52 +0900

    feat: 정답 입력 뷰 ContentView에 조립

--- SukGiSiDae/SukGiSiDae/New Group/Scene/Main/ContentView.swift
+++ SukGiSiDae/SukGiSiDae/New Group/Scene/Main/ContentView.swift
@@ -11,6 +11,10 @@ import SwiftData
 struct ContentView: View {
     @Query var gameListQuery: [Game]
     @StateObject var canvas = Canvas()
+    @State private var inputtedAnswer: String = ""
+    // "정답이에용" 대신에 실제 답 들어감
+    let answerArr: [String] = "정답이에옹".split(separator: "").map { String($0) }
+    
     var body: some View {
         //        if let game = gameListQuery.first {
         //            Text("첫 번째 퀴즈 : \(game.quizes.first ?? "")")
@@ -28,8 +32,17 @@ struct ContentView: View {
             Divider()
             ZStack {
                 CanvasView(canvas: canvas)
+                
+                HStack(spacing: 10) {
+                    ForEach(answerArr, id: \.self) { _ in
+                        InputtedAnswerView(inputtedAnswer: $inputtedAnswer)
+                    }
+                }
             }
             .background(.black)
+            
+            AnswerInputTextField(inputtedAnswer: $inputtedAnswer)
+            
         }.padding()
             .onAppear() {
                 let game = GameRepository().randomGame
