eed6dd908e825541df0ceb1bc8cf0fe06a695fc8
ChangMin Lee <sopg04@gmail.com>
2023-06-10 12:34:48 +0900

    init SukGiSiDae

---
+++ SukGiSiDae/SukGiSiDae/SukGiSiDaeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SukGiSiDaeApp.swift
+//  SukGiSiDae
+//
+//  Created by ChangMin on 2023/06/10.
+//
+
+import SwiftUI
+
+@main
+struct SukGiSiDaeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
