0131ef6c5267b9f923e163009ebb5068bedd0277
Jihyun247 <wlguslove12345@gmail.com>
2023-06-10 17:35:36 +0900

    시작뷰 보완

--- SukGiSiDae/SukGiSiDae/New Group/Scene/Main/StartView.swift
+++ SukGiSiDae/SukGiSiDae/New Group/Scene/Main/StartView.swift
@@ -11,10 +11,23 @@ struct StartView: View {
 
     var body: some View {
         ZStack {
-            Image("startStone")
-            Text("시작")
-                .font(.headline)
-        }.padding()
+            Image("stoneBackground")
+                .resizable()
+                .scaledToFill()
+                .edgesIgnoringSafeArea(.all)
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+
+            ZStack {
+                Button(action: {print("Button1")}){
+                    ZStack {
+                        Image("startStone")
+                        Text("시작")
+                            .font(.system(size: 90))
+                    }
+                }
+                .foregroundColor(.black)
+            }
+        }
     }
 }
 
